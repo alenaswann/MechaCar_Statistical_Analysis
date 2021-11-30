cc2c96c9a2ae6e6081f469e9092b0322e0119f1b
Alena <alenalswann@gmail.com>
2021-11-30 00:04:00 -0800

    Adding Deliverable 3 and images for ReadMe

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,21 @@ total_summary <- coil_table %>%
 #create lot summary dataframe
 lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+#box plot to demonstrate variance
+plt <- ggplot(coil_table,aes(x=Manufacturing_Lot,y=PSI))
+plt + geom_boxplot() + theme(axis.text.x=element_text(angle=45,hjust=1))
+
+
+#DELIVERABLE 3
+#use a t.test to determine if PSI across all lots is statistically different from population mean 1500 PSI
+t.test(coil_table$PSI,mu=1500)
+
+#use t.tests to determine if PSI across individual lots is statistically different from mean 1500 PSI
+lot1 <- subset(coil_table, Manufacturing_Lot=="Lot1")
+lot2 <- subset(coil_table, Manufacturing_Lot=="Lot2")
+lot3 <- subset(coil_table, Manufacturing_Lot=="Lot3")
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+
