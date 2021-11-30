31e9d32f65b446f499f8f7c4c715daab1c687fc6
Alena <alenalswann@gmail.com>
2021-11-29 22:11:03 -0800

    updating R file with Deliverable 2 and adding images for ReadMe

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+#DELIVERABLE 1
 #import dplyr package
 library(dplyr)
 
@@ -9,3 +10,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Use summary function to determine p-value and r-squared value for linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table))
+
+
+#DELIVERABLE 2
+#import and read mpg csv
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
+
+#create total summary dataframe
+total_summary <- coil_table %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+#create lot summary dataframe
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
